39601648fa0d1d51e589b2be456c3a4b51847b82
ashleyvernon <avernon526@gmail.com>
2016-09-22 20:52:39 -0700

    html for single card/blog

---
+++ scripts/controllers/IndexController.js
@@ -0,0 +1,35 @@
+angular
+	.module('blogApp')
+	.controller('IndexController', IndexController);
+
+IndexController.$inject = ['$http'];
+
+function IndexController($http) {
+	var vm = this;
+	vm.test = 'this is a test';
+
+	// initialize data
+
+	$http({
+		method: 'GET',
+		url: "data/blog.json"
+	}).then(onIndexSuccess, onError)
+
+	function onIndexSuccess(response){
+		console.log('request for blog data', response);
+		vm.blogs = response.data;
+	}
+
+	function onError(error) {
+		console.log('There was an error', error);
+	}
+	// getBlogs();
+
+
+	// function getBlogs() {
+	// 	BlogService.query().then(function(data){
+	// 		console.log('here\'s the blog data in the controller', data);
+	// 		vm.blogs = data;
+	// 	});
+	// }
+}
