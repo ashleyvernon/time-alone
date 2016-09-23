a528d7572903544938683a49a134553ec6f07149
ashleyvernon <avernon526@gmail.com>
2016-09-22 20:47:42 -0700

    location and route provider

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,28 @@
-angular.module('blogApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular
+	.module('blogApp', ['ngRoute'])
+	.config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+		templateUrl: '/templates/blog.html',
+		controllerAs: 'indexCtrl',
+		controller: 'IndexController'
+    })
+	.when('/:id', {
+		templateUrl: '/templates/blog.html',
+		controllerAs: 'indexCtrl',
+		controller: 'IndexController'
+	})
+    .otherwise({
+		redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+  });
+}
+
